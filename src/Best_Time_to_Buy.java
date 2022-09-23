1825e5f3266d561bd6d3eb1ad90bebfc69ca3bc3
Davit Phan <davitphan@mines.edu>
2022-09-22 23:09:18 -0600

    first attempt at java. exhaustive approach.

---
+++ src/Best_Time_to_Buy.java
@@ -0,0 +1,22 @@
+public class Best_Time_to_Buy {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+
+
+        int maxProfit = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            for (int j = i+1; j < prices.length ; j++) {
+                if (maxProfit < prices[j] - prices[i]){
+                    maxProfit = prices[j] - prices[i];
+
+                }
+
+            }
+
+        }
+        if (maxProfit < 0) return 0;
+
+        return maxProfit;
+
+    }
+}
