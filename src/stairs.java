391e463e430383942542c452c8c205e31c18bb1c
Davit Phan <davitphan@mines.edu>
2022-09-26 09:41:03 -0600

    created the iterative version of fib since recursion took too long

--- src/stairs.java
+++ src/stairs.java
@@ -15,8 +15,27 @@ public class stairs {
 
     }
 
+
+    public static int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int nSteps = 0;
+        int firstStep = 2;
+        int secondStep = 1;
+
+        for (int i = 2; i < n; i++) {
+            nSteps = firstStep + secondStep;
+            secondStep = firstStep;
+            firstStep = nSteps;
+        }
+
+
+        return nSteps;
+    }
+
     public static void main(String[] args) {
-        System.out.println(exhClimbStairs(5));
+        System.out.println(climbStairs(3));
 
 
 
